9d5e800468c2ff89f4b64d788cb2e5ab01b79ad0
Flexlug <34001155+Flexlug@users.noreply.github.com>
2022-05-15 21:55:04 +0300

    Added demo pictures

--- ResultViewer/Properties/Resources.Designer.cs
+++ ResultViewer/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ResultViewer.Properties {
     // с помощью такого средства, как ResGen или Visual Studio.
     // Чтобы добавить или удалить член, измените файл .ResX и снова запустите ResGen
     // с параметром /str или перестройте свой проект VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,16 @@ namespace ResultViewer.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap contestBarTexture {
+            get {
+                object obj = ResourceManager.GetObject("contestBarTexture", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
